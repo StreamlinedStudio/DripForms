2da9f51ecf69a4184df3fa5c803e7d9d734fee0e
Kurt Dipaolo <kurtdipaolo@gmail.com>
2017-08-28 10:41:00 -0400

    showing drip users email and tags

--- src/dripForms.js
+++ src/dripForms.js
@@ -1,20 +1,25 @@
-;(function() {
-  function identifyDripUser() {
-    return _dcq.push([
-      'identify',
-      {
-        success: function(payload) {
-          getUser(payload)
-        }
+function identifyDripUser() {
+  return _dcq.push([
+    'identify',
+    {
+      success: function(payload) {
+        getUser(payload)
       }
-    ])
-  }
+    }
+  ])
+}
 
-  identifyDripUser()
+identifyDripUser()
 
-  function getUser(user) {
-    document.querySelector('#dripFormTesting').innerHTML = user.email
-      ? 'SUBSCRIBER!!!'
-      : 'Not a subscriber!'
-  }
-})()
+function getUser(user) {
+  console.log(user) // Use DEBUGGER!
+  const profile = `
+      <h3>Subscriber!</h3>
+      <h4>${user.email}</h4>
+      <p>Tags:</p>
+      ${user.tags.map(tag => `<span>${tag}</span>`)}
+      `
+  document.querySelector('#dripFormTesting').innerHTML = user.email
+    ? profile
+    : 'Not A Subscriber'
+}
